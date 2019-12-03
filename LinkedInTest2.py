44a6ca5d69c30706f3279e05ce3e4160cbddb811
minafawzi <minafawzi@gmail.com>
2019-12-02 22:56:27 -0500

    Update LinkedInTest2.py
    updated with API handling and two visual figures

--- LinkedInTest2.py
+++ LinkedInTest2.py
@@ -23,17 +23,150 @@ dfOriginalData.shape
 dfOriginalData.to_csv("DataWODupCol.csv")
 
 #%%
+# histogram for male vs female age 
+uniqueProfiles = dfOriginalData[dfOriginalData["n_prev_tenures"]==1]
+ages_male   = uniqueProfiles[dfOriginalData["gender"] == "Male"]
+ages_female = uniqueProfiles[dfOriginalData["gender"] == "Female"]
+
+ages_male   = ages_male["age"]
+ages_female = ages_female["age"]
+
+plt.hist(ages_male, label='Ages_male',edgecolor='black', linewidth=1)
+plt.hist(ages_female, label='Ages_female',edgecolor='black', linewidth=1)
+plt.show()
+
+#%%
+fuzzyFollower = uniqueProfiles["n_followers"] + np.random.normal(0,2, size=len(uniqueProfiles["n_followers"]))
+plt.plot(uniqueProfiles["age"], fuzzyFollower, 'o', markersize=4, alpha = 1)
+plt.ylabel('Number of followers')
+plt.xlabel('Age')
+plt.show()
+
+#%%
+fuzzyage = uniqueProfiles["age"] + np.random.normal(0,1, size=len(uniqueProfiles["age"]))
+plt.plot(fuzzyage, fuzzyFollower, 'o', markersize=4, alpha = 1)
+plt.ylabel('Number of followers')
+plt.xlabel('Age')
+plt.show()
+
+#%%
+
+# the below code connects to the LinkedIn API and iterate thru the unique company name lists
+# the code will then populate the company industry dataframe that contains only three columns: 
+# 1- companyName : actual name in the dataset
+# 2- companyNameLinkedin: name used to search the linkedin 
+# 3- companyIndustry: industry retrieved from the API
+
+# After doing basic string functions, the code invokes tryCompanyIndustry that will try various options and handles 
+# the exception in the API call
 
 from linkedin_api import Linkedin
-
 linkedin = Linkedin("minafawzi.us@gmail.com", "GWU@123$$")
-results = linkedin.get_company('TT-Games-Ltd')
-#temp = results["companyIndustries"][0]["localizedName"]
-#print(temp)
-print(results["companyIndustries"])
 
-#for x in dfOriginalData.shape[0]
+#try for 50 before full 
+companyData = dfOriginalData["c_name"].head(50).unique()
+#companyData = dfOriginalData["c_name"].unique()
+companyNameList = []
+companyIndustriesList = []
+companyNameLinkedinList = []
+companyNameLinkedin=""
+companyIndustry=""
+companyName=""
 
+for i in companyData:
+    companyName = i
+    companyNameLinkedin = companyName
+    if (companyName.find("(") != -1):
+        theIndex = companyName.find("(")
+        companyNameLinkedin = companyName[0:theIndex].rstrip()
+    companyNameLinkedin = companyNameLinkedin.replace(" ","-").replace(",","")
+    print("checking for",companyNameLinkedin,"...")
+    companyIndustry =tryCompanyIndustry(linkedin,companyNameLinkedin)
+    
+    companyNameList.append(companyName)
+    companyIndustriesList.append(companyIndustry)
+    companyNameLinkedinList.append(companyNameLinkedin)
+
+print("**********Done**************")
+# %%
+# this function calls the getCompanyIndustry function and if the return is Nan then it tries 2 more variation of the name
+# the variations were analyzed with some try an error and manual search in linkedin to see how it is made
+
+# the first try is to see if the company name contains any digits, this is where the space or dash after the digits
+# will be deteted and then we re-call the API to see if it is working
+
+# the second try is to see if the first name in the company name to be used alone, i.e. Microsoft India might now work but 
+# Microsoft only will work
+
+from linkedin_api import Linkedin
+def tryCompanyIndustry(linkedinObj, companyName):
+    industry = getCompanyIndustry(linkedinObj,companyName)
+    if(industry == "Nan" and hasNumbers(companyName)):
+        print(companyName, "   Has numbers..")
+        if(companyName.find("0-")!=-1):
+            print("Found 0-")
+            companyNamecompanyName.replace("0-","0")
+            industry = getCompanyIndustry(linkedinObj,companyName)
+        if(companyName.find("1-")!=-1):
+            print("Found 1-")
+            companyName = companyName.replace("1-","1")
+            industry = getCompanyIndustry(linkedinObj,companyName)
+        if(companyName.find("2-")!=-1):
+            print("Found 2-")
+            companyName = companyName.replace("2-","2")
+            industry = getCompanyIndustry(linkedinObj,companyName)
+        if(companyName.find("3-")!=-1):
+            print("Found 3-")
+            companyName = companyName.replace("3-","3")
+            industry = getCompanyIndustry(linkedinObj,companyName)
+        if(companyName.find("4-")!=-1):
+            print("Found 4-")
+            companyName = companyName.replace("4-","4")
+            industry = getCompanyIndustry(linkedinObj,companyName)
+        if(companyName.find("5-")!=-1):
+            print("Found 5-")
+            companyName = companyName.replace("5-","5")
+            industry = getCompanyIndustry(linkedinObj,companyName)
+        if(companyName.find("6-")!=-1):
+            print("Found 6-")
+            companyName = companyName.replace("6-","6")
+            industry = getCompanyIndustry(linkedinObj,companyName)
+        if(companyName.find("7-")!=-1):
+            print("Found 7-")
+            companyName = companyName.replace("7-","7")
+            industry = getCompanyIndustry(linkedinObj,companyName)
+        if(companyName.find("8-")!=-1):
+            print("Found 8-")
+            companyName = companyName.replace("8-","8")
+            industry = getCompanyIndustry(linkedinObj,companyName)
+        if(companyName.find("9-")!=-1):
+            print("Found 9-")
+            companyName = companyName.replace("9-","9")
+            industry = getCompanyIndustry(linkedinObj,companyName)
+    if(industry == "Nan" and len(companyName.split("-"))>1):
+        print("trying first name only...")
+        industry = getCompanyIndustry(linkedinObj,companyName.split("-")[0])
+    return industry
+    
+
+#%%
+# Handy function to see if there is any digits in the string
+def hasNumbers(inputString):
+...     return any(char.isdigit() for char in inputString)
+
+
+#%%
+
+# the actuak function doing the API call
+# return JSON (if not nan) will then be searched for the specific location where indutsry is mentioned
+from linkedin_api import Linkedin
+def getCompanyIndustry(linkedinObj, companyName):
+    
+    try:
+        result = linkedinObj.get_company(companyName)["companyIndustries"][0]["localizedName"]
+        return result
+    except:
+        return "Nan"
 
 #%%
 dfOriginalData.head(5)
